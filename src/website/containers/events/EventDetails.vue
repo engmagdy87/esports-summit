7cf00fd90cfbcb32e9afb82b84f1ef4f656c146e
engmagdy87 <mohamed.magdy.abdelhamid@gmail.com>
2021-05-27 13:57:58 +0200

    remove title in event details

--- src/website/containers/events/EventDetails.vue
+++ src/website/containers/events/EventDetails.vue
@@ -164,13 +164,13 @@
           </div>
         </div>
       </div>
-      <div class="row event-details-wrapper__content__tournaments mb-5 mb-sm-3">
+      <!-- <div class="row event-details-wrapper__content__tournaments mb-5 mb-sm-3">
         <div
           class="col-12 order-2 order-md-1 col-md-6 event-details-wrapper__content__tournaments__title"
         >
           Event Tournaments
         </div>
-      </div>
+      </div> -->
       <EventsMenuView
         route="event-details"
         :data="eventDetails.tournaments"
